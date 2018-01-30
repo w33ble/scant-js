f119d810c2b28f084d72d45cd858ffaf68f4a82f
Joe Fleming <joe.fleming@gmail.com>
2018-01-29 20:40:56 -0700

    feat: initial functioning router

---
+++ packages/router/README.md
@@ -0,0 +1,6 @@
+# @scant/router
+
+Dead simple router.
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/w33ble/scant-js/master/LICENSE)
+[![npm](https://img.shields.io/npm/v/@scant/router.svg)](https://www.npmjs.com/package/@scant/router)
---
+++ packages/router/src/index.js
@@ -0,0 +1,110 @@
+import Path from 'url-pattern';
+
+// helper functions
+const isObjectLike = val => typeof val === 'object' && val != null;
+
+const hasKey = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
+
+// path format helpers
+const isValidRouteConfig = conf => {
+  if (!isObjectLike(conf)) return false;
+  const validProps = hasKey(conf, 'action') || hasKey(conf, 'children');
+  const validAction = !conf.action || typeof conf.action === 'function';
+  return hasKey(conf, 'path') && validProps && validAction;
+};
+
+const getPath = (path, appendTo = '') => {
+  if (!/^\//.test(path)) return false;
+  const newPath = `${appendTo.replace(/\/$/, '')}${path}`;
+  return newPath.length > 1 ? newPath.replace(/\/$/, '') : newPath;
+};
+
+// given a route object and optional parent path, create an array of route configs
+function parseRoute(route, path = '') {
+  if (!isValidRouteConfig(route)) {
+    throw new Error(`Route definition is invalid: ${JSON.stringify(route)}`);
+  }
+
+  const fullPath = getPath(route.path, path);
+  if (fullPath === false) throw new Error(`Route's path is invalid: ${route.path}`);
+
+  // if children is defined, recurse into child definitions, appending parent paths
+  if (Array.isArray(route.children)) {
+    return route.children.reduce(
+      (routesAcc, childRoute) => routesAcc.concat(parseRoute(childRoute, fullPath)),
+      []
+    );
+  }
+
+  return [
+    {
+      path: fullPath,
+      action: route.action,
+      name: route.name,
+      parser: new Path(fullPath),
+    },
+  ];
+}
+
+export default function createRouter(routes) {
+  if (!Array.isArray(routes)) throw new Error('An array of route objects is required');
+
+  // flatten routes into descrete objects and parsed paths
+  const routeConfigs = routes.reduce(
+    (acc, route) => {
+      parseRoute(route).forEach(({ parser, ...routeConfig }) => {
+        acc.parsers.push(parser);
+        acc.configs.push(routeConfig);
+      });
+
+      return acc;
+    },
+    { configs: [], parsers: [] }
+  );
+
+  function getRoute(url) {
+    const index = routeConfigs.parsers.findIndex(parser => parser.match(url));
+    return index < 0 ? false : { ...routeConfigs.configs[index], index };
+  }
+
+  return {
+    // given a URL, check the route collection for a match
+    match(url) {
+      const matched = getRoute(url);
+
+      // no match, nothing left to do
+      if (matched === false) return false;
+
+      // no match, nothing left to do
+      const { path, name } = matched;
+      return { path, name };
+    },
+
+    // given a URL, check routes collection, parse params & execute the action of match
+    async parse(url) {
+      const matched = getRoute(url);
+
+      // no match, nothing left to do
+      if (matched === false) return false;
+
+      // parse params and call the route handler
+      const params = routeConfigs.parsers[matched.index].match(url);
+
+      // cast any number values to numbers
+      Object.keys(params).forEach(key => {
+        const numberVal = Number.parseFloat(params[key]);
+        if (!Number.isNaN(numberVal)) params[key] = numberVal;
+      });
+
+      const payload = {
+        url,
+        match: { path: matched.path, name: matched.name },
+        router: this,
+        params,
+      };
+
+      await matched.action(payload);
+      return payload;
+    },
+  };
+}
